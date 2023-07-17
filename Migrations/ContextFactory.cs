5eb8250e021dcd1bd006a50ea999ac6757afc9a7
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-17 14:43:36 +0300

    Add files via upload

---
+++ Migrations/ContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp7.DataBaseProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.Migrations
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            optionBuilder.UseSqlServer(connectionString, assembly => assembly.MigrationsAssembly("Migrations"));
+            return new ApplicationContext(optionBuilder.Options);
+        }
+    }
+}
