cf6ce5232f7a3fec8e46375ca15284c07a7f5329
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-17 14:41:20 +0300

    Add files via upload

---
+++ Core/Info.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.Core.Models
+{
+    internal class Info
+    {
+        public int StudentNumber { get; set; }
+        public double AverageMark { get; set; }
+
+
+        public List<Person> Persons { get; set; }
+        public List<Address> Adresses { get; set; }
+    }
+}
---
+++ Core/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.Core.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime DateBirth { get; set; }
+        public char EmailAddress  { get; set; }
+
+        public List<Info> Infos { get; set; }
+        public List<Address> Adresses { get; set; }
+
+    }
+}
