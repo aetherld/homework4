630716d5258a0da576535742204281eee27b77fb
aetherld <124870915+aetherld@users.noreply.github.com>
2023-07-17 14:44:06 +0300

    Add files via upload

---
+++ DataBaseProvider/ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7.DataBaseProvider
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly string? _connectionString;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+        public ApplicationContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (_connectionString == null)
+            {
+                return;
+            }
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PersonConfiguration());
+            modelBuilder.ApplyConfiguration(new InfoConfiguration());
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
+        }
+    }
+}
